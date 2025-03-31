4c64c597fede67c3abe607be97afbc9f1f8c1041
nilesh <nileshsingh7651@hotmail.com>
2025-03-31 23:28:08 +0530

    changed import

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 import { FaLocationArrow } from "react-icons/fa6";
 
-import { socialMedia } from "@/data";
+import { socialMedia } from "../data";
 import MagicButton from "./MagicButton";
 
 const Footer = () => {
