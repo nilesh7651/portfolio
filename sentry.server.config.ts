0402ba025aaba88763eae562a3e525ab81d8424d
nilesh <nileshsingh7651@hotmail.com>
2025-03-31 23:04:35 +0530

    Removed node_modules from Git tracking

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://4133b61c5160dc3051460c2d5b2be917@o4509066577051648.ingest.us.sentry.io/4509072146563072",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
